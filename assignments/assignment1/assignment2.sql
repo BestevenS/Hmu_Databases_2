e187d12d06872292d7678f9dd2c6baca5b4631cb
Besteven <stefanostei18@gmail.com>
2023-04-17 23:45:04 +0300

    Fix some errors

--- assignments/assignment1/assignment2.sql
+++ assignments/assignment1/assignment2.sql
@@ -1,85 +1,85 @@
 -- 1
 
 SELECT * FROM users
-WHERE city = 'Ηράκλειο' AND country = 'Ελλάς';
+WHERE POLH = 'Ηράκλειο' AND XORA = 'Ελλάς';
 
 -- 2
 
-SELECT COUNT(*) FROM comments
-JOIN users ON comments.user_id = users.id
-WHERE users.city = 'Ηράκλειο' AND users.country = 'Ελλάς';
+SELECT COUNT(*) FROM coments
+JOIN users ON coments.SYG_MHNYMATOS = users.ONOMAXEIRISTH
+WHERE users.POLH = 'Ηράκλειο' AND users.XORA = 'Ελλάς';
 
 -- 3
 
-SELECT COUNT(*) FROM comments
-JOIN users ON comments.user_id = users.id
-WHERE users.country != 'Ελλάς';
+SELECT COUNT(*) FROM coments
+JOIN users ON coments.SYG_MHNYMATOS = users.ONOMAXEIRISTH
+WHERE users.XORA != 'Ελλάς';
 
 -- 4
 
-SELECT users.username, messages.content, messages.date
+SELECT users.ONOMAXEIRISTH, messages.periexomeno, messages.HMEROMHNIA
 FROM users
-JOIN messages ON users.id = messages.user_id
-ORDER BY messages.date DESC
+JOIN messages ON users.ONOMAXEIRISTH = messages.SYG_MHNYMATOS
+ORDER BY messages.HMEROMHNIA DESC
 LIMIT 1;
 
 -- 5
 
-SELECT users.username, groups.name
+SELECT users.ONOMAXEIRISTH, grups.onoma_omadas
 FROM users
-JOIN group_admins ON users.id = group_admins.user_id
-JOIN groups ON group_admins.group_id = groups.id;
+JOIN manages ON users.ONOMAXEIRISTH = manages.ONOMAXEIRISTH
+JOIN grups ON manages.onoma_omadas = grups.onoma_omadas;
 
 -- 6
 
-SELECT users.firstname, users.lastname, users.username
+SELECT users.onoma, users.EPONYMO, users.ONOMAXEIRISTH
 FROM users
-JOIN group_admins ON users.id = group_admins.user_id
-GROUP BY users.id
-ORDER BY COUNT(group_admins.group_id) DESC
+JOIN manages ON users.ONOMAXEIRISTH = manages.ONOMAXEIRISTH
+GROUP BY users.ONOMAXEIRISTH
+ORDER BY COUNT(manages.onoma_omadas) DESC
 LIMIT 1;
 
 -- 7
 
-SELECT messages.id, messages.author_id, messages.content, COUNT(likes.message_id) AS total_likes
+SELECT messages.KOD_MHNYMATOS, messages.SYG_MHNYMATOS, messages.periexomeno, COUNT(likes.KOD_MHNYMATOS) AS total_likes
 FROM messages
-JOIN likes ON messages.id = likes.message_id
-GROUP BY messages.id
-HAVING total_likes = (SELECT MAX(likes_count) FROM (SELECT COUNT(*) AS likes_count FROM likes GROUP BY message_id) AS likes_subquery);
+JOIN likes ON messages.KOD_MHNYMATOS = likes.KOD_MHNYMATOS
+GROUP BY messages.KOD_MHNYMATOS
+HAVING total_likes = (SELECT MAX(likes_count) FROM (SELECT COUNT(*) AS likes_count FROM likes GROUP BY KOD_MHNYMATOS) AS likes_subquery);
 
 -- 8
 
-SELECT messages.id, messages.author_id, messages.content, COUNT(likes.message_id) AS total_likes
+SELECT messages.KOD_MHNYMATOS, messages.SYG_MHNYMATOS, messages.periexomeno, COUNT(likes.message_id) AS total_likes
 FROM messages
-JOIN likes ON messages.id = likes.message_id
-GROUP BY messages.id
-HAVING total_likes = (SELECT MAX(likes_count) FROM (SELECT COUNT(*) AS likes_count FROM likes GROUP BY message_id) AS likes_subquery);
+JOIN likes ON messages.KOD_MHNYMATOS = likes.KOD_MHNYMATOS
+GROUP BY messages.KOD_MHNYMATOS
+HAVING total_likes = (SELECT MAX(likes_count) FROM (SELECT COUNT(*) AS likes_count FROM likes GROUP BY KOD_MHNYMATOS) AS likes_subquery);
 
 -- 9
 
-SELECT comments.user_id, comments.date
-FROM comments
-JOIN messages ON comments.message_id = messages.id
-WHERE messages.author_id = {USER_ID_OF_YOUR_CHOICE};
+SELECT coments.SYG_MHNYMATOS, coments.HMEROMHNIA
+FROM coments
+JOIN messages ON coments.KOD_MHNYMATOS = messages.KOD_MHNYMATOS
+WHERE messages.SYG_MHNYMATOS = 1;
 
 -- 10
 
 SELECT * FROM users
-WHERE id NOT IN (SELECT DISTINCT follower_id FROM followers);
+WHERE id NOT IN (SELECT DISTINCT ONOMAXEIRISTH FROM FOLOWS);
 
 -- 11
 
-SELECT messages.author_id, messages.id, COUNT(comments.message_id) AS comment_count
+SELECT messages.SYG_MHNYMATOS, messages.KOD_MHNYMATOS, COUNT(coments.KOD_MHNYMATOS) AS comment_count
 FROM messages
-JOIN comments ON messages.id = comments.message_id
-GROUP BY messages.id
+JOIN coments ON messages.KOD_MHNYMATOS = coments.KOD_MHNYMATOS
+GROUP BY messages.KOD_MHNYMATOS
 HAVING comment_count >= 1;
 
 -- 12
 
-SELECT users.username, users.firstname, users.lastname, COUNT(followers.follower_id) AS followers_count
+SELECT users.ONOMAXEIRISTH, users.ONOMA, users.EPONYMO, COUNT(FOLOWS.ONOMAXEIRISTH) AS followers_count
 FROM users
-JOIN followers ON users.id = followers.followed_id
-GROUP BY users.id
+JOIN FOLOWS ON users.ONOMAXEIRISTH = FOLOWS.ONOMAXEIRISTH
+GROUP BY users.ONOMAXEIRISTH
 ORDER BY followers_count DESC
 LIMIT 1;
