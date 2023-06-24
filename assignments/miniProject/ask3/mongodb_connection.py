6648e49b30767618100cc50fa503f1cb5799a6fb
Besteven <stefanostei18@gmail.com>
2023-06-24 12:05:04 +0300

    ask1, ask2 ready

--- assignments/miniProject/ask3/mongodb_connection.py
+++ assignments/miniProject/ask3/mongodb_connection.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")  # Αντικαταστήστε με το δικό σας URI αν είναι απαραίτητο
-db = client['yourDBname']  # Αντικαταστήστε με το όνομα της βάσης δεδομένων σας
+db = client['mydb']  # Αντικαταστήστε με το όνομα της βάσης δεδομένων σας
